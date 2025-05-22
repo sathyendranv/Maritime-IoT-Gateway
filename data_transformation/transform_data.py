debc5d06399f38dfc198736d9a41eb3b7a6d0848
Vellaisamy, Sathyendran <sathyendran.vellaisamy@intel.com>
2025-05-22 17:58:52 +0530

    DT: Added logic for check offset and publishing
    Signed-off-by: Vellaisamy, Sathyendran <sathya6045@gmail.com>

--- data_transformation/transform_data.py
+++ data_transformation/transform_data.py
@@ -9,11 +9,14 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from common.common import MsgTransfer
 
 class DataTransformer:
-    def __init__(self, logger, q, write_q):
+    def __init__(self, logger, q, write_q, syncInterval, offset):
         self.data_published = {}
+        self.last_published = {}
         self.logger = logger
         self.q = q
         self.write_q = write_q
+        self.syncInterval = syncInterval
+        self.offset = offset
 
     def generate_publish_msg(self, data, status, unit,timestamp):
         status_msg = "Invalid"
@@ -29,17 +32,32 @@ class DataTransformer:
 
         payload = str(data) + unit_str + ", " + status_msg + ", "+ timestamp
         return payload
+    def check_if_data_published(self, data, topic):
+        if self.offset == 0:
+            return True
+        if topic not in self.data_published:
+            self.data_published[topic] = data
+            self.last_published[topic] = time.time()
+            return True
+        if abs(float(data) - float(self.data_published[topic])) > self.offset:
+            self.data_published[topic] = data
+            self.last_published[topic] = time.time()
+            return True
+        return False
+
+
     def worker(self):
         while True:
             try:
                 # Get an item from the queue
                 item = self.q.get(timeout=3)  # Wait for 3 seconds for an item
                 msg = self.generate_publish_msg(item.data, item.status, item.unit, item.timestamp)
-
-                obj = MsgTransfer("", msg, item.publich_topic)
-                self.write_q.put(obj)
-                self.logger.debug(f"Transformed data: {obj.__dict__}")
-
+                if self.check_if_data_published(item.data, item.publich_topic):
+                    obj = MsgTransfer("", msg, item.publich_topic)
+                    self.write_q.put(obj)
+                    self.logger.debug(f"Transformed data: {obj.__dict__}")
+                else:
+                    self.logger.info(f"Data not published due to offset: {item.data} - {self.data_published[item.publich_topic]} topic: {item.publich_topic}")
                 # Simulate processing time
                 time.sleep(0.1)
                 self.logger.debug(f"Finished processing item: {item}")
@@ -52,5 +70,5 @@ class DataTransformer:
                 
 
 def start_data_transformer(config, logger, read_q, write_q):
-    transformer = DataTransformer(logger, read_q, write_q)
+    transformer = DataTransformer(logger, read_q, write_q, config['syncInterval'], config['offset'])
     transformer.worker()
