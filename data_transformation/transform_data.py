2cc455cdef9bbdc6c6ad04aca87aa69e73e2de68
Vellaisamy, Sathyendran <sathyendran.vellaisamy@intel.com>
2025-05-22 18:11:48 +0530

    Transformer: Added logic to sync interval if not sent in x interval
    Signed-off-by: Vellaisamy, Sathyendran <sathya6045@gmail.com>

--- data_transformation/transform_data.py
+++ data_transformation/transform_data.py
@@ -2,11 +2,18 @@ import time
 import queue
 import sys
 import os
+import threading
 
 # Add the top-level directory to sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from common.common import MsgTransfer
+from common.common import MsgTransfer, DataTransfer
+
+class DataBackup(DataTransfer):
+    def __init__(self, item, timestamp):
+        super().__init__(item.data, timestamp, item.publich_topic, item.unit, item.status)
+        self.timestamp = timestamp
+
 
 class DataTransformer:
     def __init__(self, logger, q, write_q, syncInterval, offset):
@@ -32,16 +39,18 @@ class DataTransformer:
 
         payload = str(data) + unit_str + ", " + status_msg + ", "+ timestamp
         return payload
-    def check_if_data_published(self, data, topic):
+    def check_if_data_published(self, item, topic):
         if self.offset == 0:
             return True
         if topic not in self.data_published:
-            self.data_published[topic] = data
-            self.last_published[topic] = time.time()
+            self.data_published[topic] = item.data
+            obj = DataBackup(item, time.time())
+            self.last_published[topic] = obj
             return True
-        if abs(float(data) - float(self.data_published[topic])) > self.offset:
-            self.data_published[topic] = data
-            self.last_published[topic] = time.time()
+        if abs(float(item.data) - float(self.data_published[topic])) > self.offset:
+            self.data_published[topic] = item.data
+            obj = DataBackup(item, time.time())
+            self.last_published[topic] = obj
             return True
         return False
 
@@ -52,7 +61,7 @@ class DataTransformer:
                 # Get an item from the queue
                 item = self.q.get(timeout=3)  # Wait for 3 seconds for an item
                 msg = self.generate_publish_msg(item.data, item.status, item.unit, item.timestamp)
-                if self.check_if_data_published(item.data, item.publich_topic):
+                if self.check_if_data_published(item, item.publich_topic):
                     obj = MsgTransfer("", msg, item.publich_topic)
                     self.write_q.put(obj)
                     self.logger.debug(f"Transformed data: {obj.__dict__}")
@@ -67,8 +76,21 @@ class DataTransformer:
                 print("No more items to process, worker is exiting.")
                 time.sleep(1)
                 continue
-                
+
+def sync_data_publisher(transformer_obj):
+    while True:
+        current_time = time.time()
+        for topic, item in transformer_obj.last_published.items():
+            if current_time - item.timestamp > transformer_obj.syncInterval:
+                msg = transformer_obj.generate_publish_msg(item.data, item.status, item.unit, item.timestamp)
+                obj = MsgTransfer("", msg, topic)
+                transformer_obj.write_q.put(obj)
+                transformer_obj.logger.debug(f"Sync published data: {obj.__dict__}")
+        time.sleep(transformer_obj.syncInterval)
+               
 
 def start_data_transformer(config, logger, read_q, write_q):
     transformer = DataTransformer(logger, read_q, write_q, config['syncInterval'], config['offset'])
+    t = threading.Thread(target=sync_data_publisher, args=(transformer,))
+    t.start()     
     transformer.worker()
